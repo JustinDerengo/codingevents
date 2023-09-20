06c6aa6df98edde874b5ea1b5f703ff714d129b7
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-09-20 12:51:27 -0600

    Refactor for model, model binding

--- codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
+++ codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,20 +1,38 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 public class Event {
 
     private int id;
     private static int nextID = 1;
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
+    @Size(max = 500, message = "Description too long!")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    @NotBlank(message = "Location cannot be left blank.")
+    private String location;
+
+
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextID;
         nextID++;
     }
+    public Event() {}
 
     public String getName() {
         return name;
@@ -30,6 +48,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
