d65da682df05f3951db60b78d3937562790db4b8
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-09-21 09:40:33 -0600

    Errors with "Bad data"

--- codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
+++ codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -10,11 +10,12 @@ public class Event {
 
     private int id;
     private static int nextID = 1;
-    @NotBlank
+    @NotBlank(message = "Name is required.")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
     @Size(max = 500, message = "Description too long!")
     private String description;
+    @NotBlank(message = "Email is required")
     @Email(message = "Invalid email. Try Again.")
     private String contactEmail;
 
